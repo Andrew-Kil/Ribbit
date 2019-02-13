af7fc9094dab4d988c35f950ffe2e89b982537a3
Andrew KIl <kil.andrew@gmail.com>
2019-02-12 19:58:57 -0500

    update navbar

--- frontend/src/components/NavBar/NavBar.js
+++ frontend/src/components/NavBar/NavBar.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import "./NavBar.css";
+
 import logo from "./logo.png";
+import frog from "./frog.png";
+import magnifying_glass from "./magnifying_glass.png";
 
 export default class NavBar extends Component {
   state = {
@@ -9,14 +12,26 @@ export default class NavBar extends Component {
   render() {
     return (
       <div className="navbar">
-        <img src={logo} alt="logo" className="logo" height="50" width="120" />
-        <input
-          type="text"
-          value={this.state.searchInput}
-          size="40"
-          placeholder="Search Ribbit"
-          className="searchBar"
-        />
+        <a href="/">
+          <img src={frog} alt="frog" className="frog" height="50" width="50" />
+          <img src={logo} alt="logo" className="logo" height="50" width="120" />
+        </a>
+        <div>
+          <img
+            src={magnifying_glass}
+            alt="magnifying glass"
+            className="magnifying_glass"
+            height="30"
+            width="30"
+          />
+          <input
+            type="text"
+            value={this.state.searchInput}
+            size="40"
+            placeholder="Search Ribbit"
+            className="searchBar"
+          />
+        </div>
       </div>
     );
   }
