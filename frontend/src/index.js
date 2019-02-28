8aba50cc21d37cdfbf4c899fc1f090c24c061f0b
Andrew KIl <kil.andrew@gmail.com>
2019-02-28 11:10:05 -0500

    add package.json

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,10 +5,61 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
 
+import { combineReducers, createStore } from "redux";
+import { Provider } from "react-redux";
+
+const productsReducer = (state = [], action) => {
+  return state;
+};
+
+const userReducer = (state = "", { type, payload }) => {
+  switch (type) {
+    case "updateUser":
+      return payload;
+  }
+  return state;
+};
+
+const allReducers = combineReducers({
+  products: productsReducer,
+  user: userReducer
+});
+
+const store = createStore(
+  allReducers,
+  {
+    products: [{ name: "iPhone" }],
+    user: "Michael"
+  },
+  window.devToolsExtension && window.devToolsExtension()
+);
+
+const updateUserAction = {
+  type: "updateUser",
+  payload: {
+    user: "John"
+  }
+};
+
+store.dispatch(updateUserAction);
+
+console.log(store.getState());
+
+// const action = {
+//   type: "changeState",
+//   payload: {
+//     newState: "New state"
+//   }
+// };
+
+console.log(store.getState());
+
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById("root")
 );
 
