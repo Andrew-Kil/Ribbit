2e630b3f5aa4783bdc3f860f1b74ad299183825c
Andrew KIl <kil.andrew@gmail.com>
2019-03-04 17:27:03 -0500

    import ads and display a random ad when the page is refreshed

--- frontend/src/components/SideBar/AdSideBar/AdSideBar.js
+++ frontend/src/components/SideBar/AdSideBar/AdSideBar.js
@@ -2,7 +2,19 @@ import React from "react";
 
 import "./AdSideBar.css";
 
-import ad from "./geico-ad.jpg";
+import geico from "./geico-ad.jpg";
+import snickers from "./snickers-ad.jpg";
+import tobasco from "./tobasco-ad.jpg";
+import starbucks from "./starbucks-ad.png";
+import mcdonalds from "./mcdonalds-ad.jpg";
+import coke_zero from "./coke-zero-ad.jpeg";
+import adidas from "./adidas.jpg";
+
+const ads = [geico, snickers, tobasco, starbucks, mcdonalds, coke_zero, adidas];
+const randomAd = ads[Math.floor(Math.random() * ads.length)];
+
+console.log(Math.floor(Math.random() * 2));
+// # from 0-1
 
 export default function AdSideBar() {
   return (
@@ -10,9 +22,13 @@ export default function AdSideBar() {
       <div className="ad-side-bar">
         <div className="ad-side-bar-padding">
           <div className="ad-side-bar-body">
-            <div className="ad-side-bar-body-header">ADVERTISEMENT</div>
+            <span className="ad-side-bar-body-header">ADVERTISEMENT</span>
+            <span className="ad-side-bar-body-text">
+              (refresh to see new ad)
+            </span>
             <br />
-            <img src={ad} alt="geico ad" className="ad" />
+            <br />
+            <img src={randomAd} alt="geico ad" className="ad" />
           </div>
         </div>
       </div>
