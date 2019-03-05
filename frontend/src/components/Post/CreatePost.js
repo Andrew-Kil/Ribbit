aa3d1783f5788dd7dc1d3f05aae94a70018b2258
Andrew KIl <kil.andrew@gmail.com>
2019-03-05 15:09:13 -0500

    create post: works

--- frontend/src/components/Post/CreatePost.js
+++ frontend/src/components/Post/CreatePost.js
@@ -17,76 +17,23 @@ export default class CreatePost extends Component {
 
   handleSubmit = e => {
     e.preventDefault();
-    console.log("form submitted");
-
-    const postInfo = {
-      title: this.state.title,
-      body: this.state.body,
-      user_id: this.state.user_id,
-      sub_id: this.state.sub_id
-    };
 
     axios
-      .post("/posts/create", { postInfo })
+      .post("/posts/create", {
+        title: this.state.title,
+        body: this.state.body,
+        user_id: this.state.user_id,
+        sub_id: this.state.sub_id
+      })
       .then(res => {
-        this.setState({ posts: res.data.data });
+        console.log(res);
+        console.log(res.data);
       })
       .catch(err => console.log(err));
-
-    // fetch("/posts/create", {
-    //   method: "POST",
-    //   headers: {
-    //     Accept: "application/json",
-    //     "Content-Type": "application/json"
-    //   },
-    //   body: JSON.stringify({
-    //     title: this.state.title,
-    //     body: this.state.body,
-    //     user_id: this.state.user_id,
-    //     sub_id: this.state.sub_id
-    //   })
-    // });
   };
-
-  //   handleSubmit = (e, data) => {
-  //     e.preventDefault();
-  //     console.log("form submitted");
-
-  //     function createPost(data) {
-  //       return fetch("./posts/create", {
-  //         method: "POST",
-  //         mode: "CORS",
-  //         body: JSON.stringify(data),
-  //         headers: {
-  //           "Content-Type": "application/json"
-  //         }
-  //       })
-  //         .then(res => {
-  //           return res;
-  //         })
-  //         .catch(err => err);
-  //     }
-
-  //     createPost(data);
-  //   };
 
   render() {
     console.log(this.state);
-
-    // function createPost(data) {
-    //   return fetch("./posts/create", {
-    //     method: "POST",
-    //     mode: "CORS",
-    //     body: JSON.stringify(data),
-    //     headers: {
-    //       "Content-Type": "application/json"
-    //     }
-    //   })
-    //     .then(res => {
-    //       return res;
-    //     })
-    //     .catch(err => err);
-    // }
 
     return (
       <div className="form-container">
